425eaa9213cd8bfedc8aafd731fdbfde76f8246e
Aidar Yessembayev <yessembayevaidar@gmail.com>
2022-08-05 01:06:12 +0600

    init

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,10 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int f = number / 1000;
+        int s = (number / 100) % 10;
+        int t = (number / 10) % 100 % 10;
+        int fo = number % 1000 % 100 % 10;
+        System.out.println(f+s+t+fo);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,9 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int last = number / 100;
+        int second = (number / 10) % 10;
+        int first = (number % 100) % 10;
+        System.out.println("" + first + second + last);
     }
 }
